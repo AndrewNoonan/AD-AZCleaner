21069f7697ae90ab5c8d8adefdf69b974e25c28d
AndrewNoonan <andrewnoonan3@gmail.com>
2023-12-22 07:59:32 -0500

    New branch.

--- AD-AZCleaner.ps1
+++ AD-AZCleaner.ps1
@@ -1,25 +1,42 @@
-$prefixs = "MHL-", "MSL-", "MHDK-", "MSDK-"
+#set-executionpolicy -ExecutionPolicy RemoteSigned -Scope Process
+#Connect-AzureAD
+$prefixes = "MHD-","MHL-","MHDK-", "MSDC-", "MSD", "MSL-", "MSDK-"
 
-foreach ($machine in Get-Content .\File.txt*) {
+foreach ($machine in Get-Content .\Devices.txt) {
     $count = 0
-    foreach ($prefix in $prefixs) {
-            if ($machine.Length -gt 8) {
-                $machine = $machine.Substring(12)
-            }
+    $temp = ""
+    if ($machine.Length -gt 8) {
+        $machine = $machine.Substring(12)
+    }
+    foreach ($prefix in $prefixes) {
             try {
                 $temp = $prefix + $machine
+                Get-ADComputer -Identity $temp
                 if (Get-ADComputer -Identity $temp) {
                     if (Remove-ADComputer -Identity $temp) {
-                        Write-Host $temp": AD record deleted"
+                        Write-Host $machine": AD record deleted"
                     } else {
-                        Write-Host $temp": AD record deletion skipped"
+                        Write-Host $machine": AD record deletion skipped"
+                        $temp2 = $temp
                     }
                 }
-            } catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]{
-                $count++
-                if ($count -ge $prefixs.Length){
+            } catch {
+                if ($count -ge $prefixes.Length - 1){
                     Write-Host $machine": AD record not found"
                 }
+                $count++
             }
+    }
+    if ((Get-AzureADDevice -SearchString $machine) -or (Get-AzureADDevice -SearchString $temp)) {
+        $input = Read-Host "Input machine name to delete azure record"
+        if (($machine -contains $input) -or ($temp2 -contains $input)) {
+            Get-AzureADDevice -SearchString $input | Remove-AzureADDevice
+            Write-Host $machine": Azure record deleted"
+        } else {
+            Write-Host $machine": Azure record deletion skipped"
         }
+    } else {
+        Write-Host $machine": Azure record not found"
+    }
+    Write-Host ""
 }
