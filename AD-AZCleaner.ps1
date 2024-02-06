f1ef08cc12de819d235057f7eefdf48b4dc97ce8
AndrewNoonan <andrewnoonan3@gmail.com>
2024-02-06 15:44:21 -0500

    Rewrote entire script for clarity & adding in InTune removal function. Script is not functional in terms of ability to delete entities. Said function will be added after extensive testing.

--- AD-AZCleaner.ps1
+++ AD-AZCleaner.ps1
@@ -1,62 +1,73 @@
+$padding = "     "
+$prefixes = "","MHD-","MHL-","MHDK-", "MSDC-", "MSD-", "MSL-", "MSDK-"
+$InTuneDevices = Get-MgDeviceManagementManagedDevice -All | Select-Object DeviceName, Id, UserId
+Connect-MgGraph
 Connect-AzureAD
-$prefixes = "MHD-","MHL-","MHDK-", "MSDC-", "MSD-", "MSL-", "MSDK-"
 
-foreach ($SN in Get-Content .\CleanerDevices.txt) {
+foreach ($SN in Get-Content .\test.txt) {
+    Write-Host $padding"--- $SN ---"$padding -ForegroundColor Cyan
     $count = 0
-    $temp = ""
-    if ($SN.Length -gt 8) {
-        $SN = $SN.Substring(12)
+    foreach($device in $InTuneDevices) {
+        foreach($prefix in $prefixes) {
+            $machine = $prefix + $SN
+            if ($device.DeviceName -eq $machine) {
+                Write-Host "InTune| DELETING $machine" -ForegroundColor Red;
+                if ((Read-Host $padding"$machine (y/n)") -eq 'y') {
+                    #Insert deletion statement here
+                    #Write-Host $padding$Machine"| InTune record deleted" -ForegroundColor Green
+                    Write-Host $device.DeviceName, $device.Id, $device.UserId -ForegroundColor Green
+                } else {
+                    Write-Host $padding$SN"| InTune record deletion skipped" -ForegroundColor Yellow
+                }                  
+            } else {
+                #Write-Host ($Count/($prefixes.Length))
+                $count++ 
+                if (($Count/($prefixes.Length)) -ge $InTuneDevices.Count) {
+                    Write-Host $padding$SN"| InTune record not found" -ForegroundColor Yellow
+                }
+            }
+        }
     }
-
-    #AD
-
+    $count = 0
     foreach ($prefix in $prefixes) {
         $machine = $prefix + $SN
         try {
             if (Get-ADComputer -Identity $machine) {
-                if ((Read-Host "$machine delete AD record (y/n): ") -eq 'y') {
-                    Remove-ADComputer -Identity $machine -Confirm:$false
-                    Write-Host $machine": AD record deleted"
+                Write-Host "AD| DELETING $machine" -ForegroundColor Red
+                if ((Read-Host $padding$machine" delete AD record (y/n): ") -eq 'y') {
+                    #Remove-ADComputer -Identity $machine -Confirm:$false
+                    Write-Host $padding$machine"| AD record deleted" -ForegroundColor Green
                 } else {
-                    Write-Host $machine": AD record deletion skipped"
+                    Write-Host $padding$machine"| AD record deletion skipped" -ForegroundColor Yellow
                 }
-            } else {
-                Write-Host $SN": No AD record found"
             }
         } catch {
-            if ($count -ge $prefixes.Length - 1){
-                Write-Host $SN": AD record not found"
-            }
             $count++
+            if (($count/($prefixes.Length)) -ge $SN.Count){
+                Write-Host $padding$SN"| AD record not found" -ForegroundColor Yellow
+            }
         }
     }
-
-    #Azure
-
     $count = 0
     foreach ($prefix in $prefixes) {
         $machine = $prefix + $SN
-        if (Get-AzureADDevice -SearchString $SN) {
-            if ((Read-Host "$SN delete Azure record (y/n)") -eq 'y') {
-                Get-AzureADDevice -SearchString $SN | Remove-AzureADDevice
-                Write-Host $SN": Azure record deleted"
-            } else {
-                Write-Host $SN": AD record deletion skipped"
-            }  
-        } elseif (Get-AzureADDevice -SearchString $machine) {
-            if ((Read-Host "$machine delete Azure record (y/n)") -eq 'y') {
-                Get-AzureADDevice -SearchString $machine | Remove-AzureADDevice
-                Write-Host $machine": Azure record deleted"
-            } else {
-                Write-Host $machine": AD record deletion skipped"
+        try {
+            if (Get-AzureADDevice -SearchString $machine ) {
+                Write-Host "EntraID| DELETING $machine" -ForegroundColor Red
+                if ((Read-Host "$padding$SN delete Azure record (y/n)") -eq 'y') {
+                    #Get-AzureADDevice -SearchString $SN | Remove-AzureADDevice
+                    Write-Host $padding$SN"| EntraID record deleted" -ForegroundColor Green
+                } else {
+                    Write-Host $padding$SN": EntraID record deletion skipped" -ForegroundColor Yellow
+                }  
             }
-        } else {
-            if ($count -ge $prefixes.Length - 1){
-                Write-Host $SN": Azure record not found"
-            }
+        } catch {
             $count++
+            if (($count/($prefixes.Length)) -ge $SN.Count){
+                Write-Host $padding$SN"| EntraID record not found" -ForegroundColor Yellow
+            }
         }
     }
-
-    Write-Host ""
 }
+#NOTES:
+#Use ($Count/($prefixes.Length)) to calculate what device/total the current loop is on.
---
+++ InTuneSift.ps1
@@ -0,0 +1,16 @@
+﻿#Sign in with administrative account.
+Connect-MgGraph
+
+Write-Host "Collecting devices..."
+$InTuneDevices = Get-MgDeviceManagementManagedDevice -All | Select-Object DeviceName, Id, UserId
+
+Write-Host "Populating InTune folder..."
+$InTuneDevices.DeviceName | Out-File -Filepath ".\InTune\DeviceSNs.txt"
+$InTuneDevices.Id | Out-File -Filepath ".\InTune\DeviceIDs.txt"
+$InTuneDevices.UserId | Out-File -Filepath ".\InTune\DeviceUserIDs.txt"
+
+foreach ($SN in $InTuneDeviceSNs) {
+    if ($SN.DeviceName -eq "MSL-PW04B8NT") {
+        Write-Host $SN.DeviceName, $SN.Id, $SN.UserId
+    }
+}
